cff3368cb54b30bd29c98a7f2dfb1ba40b9ce0e8
Xiaowei Xue <xiaowei.xue@shopee.com>
2023-07-30 23:41:08 +0800

    chore: fix ci

--- src/rules/example-rule.ts
+++ src/rules/example-rule.ts
@@ -1,9 +1,11 @@
-import { TSESLint } from '@typescript-eslint/experimental-utils';
+import { TSESLint } from '@typescript-eslint/utils';
 
 const rule: TSESLint.RuleModule<'disallowExample', []> = {
   meta: {
     docs: {
       description: 'An example rule.',
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       category: 'Stylistic Issues',
       recommended: 'warn',
       url: 'https://github.com/kotarella1110/template-typescript-eslint-plugin/blob/master/docs/rules/example-rule.md',
